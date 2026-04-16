9741b6bfd43287be5acaca6c2e71648754cc72f3
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-16 23:18:58 +0530

    using multer

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
-
+const multer = require('multer')
+const postModel = require('./models/post.model')
 const app = express();
+
+// middleware
+app.use(express.json());
+
+const upload = multer({storage: multer.memoryStorage()})
+
+app.post('/create-post',upload.single('image') , async (req, res)=> {
+    const data = req.body
+
+    console.log(data);
+    console.log(req.file);
+
+})
 
 module.exports = app
